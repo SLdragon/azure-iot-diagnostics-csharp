0ccc8151dacc4304c5b865aa95c3120db12f109a
Erich Wang <eriwan@microsoft.com>
2017-06-06 15:59:22 +0800

    update sample for bug bash

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -1,16 +1,17 @@
-﻿using System;
+﻿using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Client.DiagnosticProvider;
+using Newtonsoft.Json;
+using System;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Azure.Devices.Client.DiagnosticProvider;
-using Microsoft.Azure.Devices.Client;
-using Newtonsoft.Json;
 
 namespace Sample
 {
     class Program
     {
-        private static readonly string deviceConnectionString = "{Your IoTHub device connection string}";
+        private static readonly string deviceConnectionString = "HostName=erich-testiotf1.azure-devices.net;DeviceId=test1;SharedAccessKey=P15BgaE2NkIFhAd6RKkI9gfNFgOJYA+H/O06xmY4/L4=";
+        private static volatile bool sendInvalidMessage = false;
         public static void Main(string[] args)
         {
             SendD2CMessage();
@@ -20,26 +21,35 @@ namespace Sample
         {
             var tokenSource = new CancellationTokenSource();
 
-            Console.CancelKeyPress += (s, e) =>
+            Task.Run(async() =>
             {
-                e.Cancel = true;
-                tokenSource.Cancel();
-                Console.WriteLine("Existing ...");
-            };
-            Console.WriteLine("Press CTRL+C to exit");
+                await SendDeviceToCloudMessageAsync(tokenSource.Token);
+            });
 
-            Task.WaitAll(
-                SendDeviceToCloudMessageAsync(tokenSource.Token)
-                );
+            Console.WriteLine("Press Y to send invalid messages, press any other key to exit");
+            bool shouldContinue = true;
+            while (shouldContinue)
+            {
+                var key = Console.ReadKey();
+                switch(key.Key)
+                {
+                    case ConsoleKey.Y:
+                        sendInvalidMessage = true;
+                        Console.WriteLine("Sending invalid message...");
+                        break;
+                    default:
+                        tokenSource.Cancel();
+                        Thread.Sleep(1000);
+                        shouldContinue = false;
+                        break;
+                }
+            }
         }
 
         public static async Task SendDeviceToCloudMessageAsync(CancellationToken cancelToken)
         {
-
-            var diagnosticProvider = new ProbabilityDiagnosticProvider(SamplingRateSource.Server, 50);
+            var diagnosticProvider = new ProbabilityDiagnosticProvider(SamplingRateSource.Server, 0);
             var deviceClient = DeviceClientWrapper.CreateFromConnectionString(deviceConnectionString, diagnosticProvider);
-
-            const int avgWindSpeed = 10; // m/s
             var rand = new Random();
 
             while (true)
@@ -47,18 +57,31 @@ namespace Sample
                 if (cancelToken.IsCancellationRequested)
                     break;
 
-                var currentWindSpeed = avgWindSpeed + rand.NextDouble() * 4 - 2;
+                var randomHumity = rand.Next(0, 100);
 
-                var telemetryDataPoint = new
+                dynamic telemetryDataPoint;
+
+                if(sendInvalidMessage)
                 {
-                    windSpeed = currentWindSpeed
-                };
+                    telemetryDataPoint = new
+                    {
+                        humity = randomHumity
+                    };
+                }
+                else
+                {
+                    telemetryDataPoint = new
+                    {
+                        temperature = rand.Next(0, 40),
+                        humity = randomHumity
+                    };
+                }
 
                 var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
                 var message = new Message(Encoding.ASCII.GetBytes(messageString));
                 await deviceClient.SendEventAsync(message);
                 Console.WriteLine("{0} > Sending message: {1} | Count:{2}", DateTime.Now, messageString, diagnosticProvider.MessageNumber);
-                await Task.Delay(500, cancelToken);
+                await Task.Delay(1000, cancelToken);
             }
         }
     }
